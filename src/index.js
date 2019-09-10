29d9540405481c7b1165ff5351c6aa24c21d075c
Pasha Smolov <palexsmol@gmail.com>
2019-09-10 15:32:56 -0700

    init and redux ui

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INC":
+      return state + 1;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    case "DEC":
+      return state - 1;
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+document.getElementById("inc").addEventListener("click", () => {
+  store.dispatch({ type: "INC" });
+});
+document.getElementById("dec").addEventListener("click", () => {
+  store.dispatch({ type: "DEC" });
+});
+const update = () => {
+  document.getElementById("counter").innerHTML = store.getState();
+};
+
+store.subscribe(update);
