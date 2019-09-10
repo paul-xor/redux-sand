1557bc8862c5ef1f4f75cf9ab6d81644c8ab5279
Pasha Smolov <palexsmol@gmail.com>
2019-09-10 16:33:21 -0700

    bindActionCreators

--- src/index.js
+++ src/index.js
@@ -1,37 +1,20 @@
-import { createStore } from "redux";
-
-const reducer = (state = 0, action) => {
-  switch (action.type) {
-    case "RND":
-      return state + action.payload;
-
-    case "INC":
-      return state + 1;
-
-    case "DEC":
-      return state - 1;
-
-    default:
-      return state;
-  }
-};
+import { createStore, bindActionCreators } from "redux";
+import reducer from "./reducer";
+import * as actions from "./actions";
 
 const store = createStore(reducer);
+const { dispatch } = store;
 
-const inc = () => ({ type: "INC" });
-const dec = () => ({ type: "DEC" });
-const rnd = payload => ({ type: "RND", payload });
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
-document.getElementById("inc").addEventListener("click", () => {
-  store.dispatch(inc());
-});
-document.getElementById("dec").addEventListener("click", () => {
-  store.dispatch(dec());
-});
+document.getElementById("inc").addEventListener("click", inc);
+document.getElementById("dec").addEventListener("click", dec);
+
 document.getElementById("rnd").addEventListener("click", () => {
   const payload = Math.floor(Math.random() * 10);
-  store.dispatch(rnd(payload));
+  rnd(payload);
 });
+
 const update = () => {
   document.getElementById("counter").innerHTML = store.getState();
 };
