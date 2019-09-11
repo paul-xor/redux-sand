52c34233552b9c7c5507bb38e727dc57a829d918
Pasha Smolov <palexsmol@gmail.com>
2019-09-10 18:04:08 -0700

    mapDispatchToProps as an object

--- src/components/counter.js
+++ src/components/counter.js
@@ -26,19 +26,7 @@ const mapStateToProps = state => {
   };
 };
 
-const mapDispatchToProps = dispatch => {
-  const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
-  return {
-    inc,
-    dec,
-    rnd: () => {
-      const randomValue = Math.floor(Math.random() * 10);
-      rnd(randomValue);
-    }
-  };
-};
-
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  actions
 )(Counter);
